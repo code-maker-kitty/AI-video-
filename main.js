90709109f85ab12c98d6ca041c1c8d8aa0743dff
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-09-30 16:32:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects =[];
 video ="";
 status ="";
 function preload(){
@@ -10,6 +11,20 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status != ""){
+      objectDetector.detect(video, gotResults);
+      for (i = 0; i < objects.length; i++){
+          document.getElementById("status").innerHTML = "Status : Objects Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+          fill("lime");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("lime");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+    }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
@@ -21,4 +36,11 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResults(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
